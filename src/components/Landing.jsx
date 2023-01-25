8c0400f97101d01fc1aba5e8066aad567378f654
hamzotta <hamzakamal987@gmail.com>
2023-01-25 04:18:05 +0100

    modified the browse book button

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,7 +11,7 @@ import UndrawBooks from '../assets/Undraw_Books.svg'
                 <div className="header__description">
                     <h1>Tunisia's most awarded online library platform</h1>
                     <h2>Find your dream book with <span className='purple'>Library</span></h2>
-                    <Link to="#features">
+                    <Link to="/books">
                         <button className='btn'>Browse books</button>
                     </Link>
                 </div>
